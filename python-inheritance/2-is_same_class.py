ce367ad2d2c575fdbb3b929505a81661cab26697
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-05-29 11:27:06 +0000

    Task 2: Check if object is exactly an instance of a given class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to check if object is exactly an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if obj is exactly an instance of a_class"""
+    return type(obj) == a_class
