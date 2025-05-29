9c7d423b144bddd31eb75d900c5fbcf8a368b043
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-05-29 11:09:34 +0000

    Task 1: Add MyList class in my_list_1.py and doctest

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""MyList module that inherits from built-in list"""
+
+
+class MyList(list):
+    """Custom list class with additional method to print sorted list"""
+
+    def print_sorted(self):
+        """Prints the list in ascending sorted order"""
+        print(sorted(self))
