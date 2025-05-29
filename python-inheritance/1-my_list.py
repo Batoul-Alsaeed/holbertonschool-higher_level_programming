c072c3cdceec4f0870ef6c3bfbb074ded4da20a1
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-05-29 17:39:33 +0000

    Fix PEP8: Adjust docstrings formatting in MyList

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""MyList module that inherits from built-in list"""
+"""
+MyList module that inherits from built-in list
+"""
 
 
 class MyList(list):
-    """Custom list class with additional method to print sorted list"""
+    """
+    Custom list class with additional method to print sorted list
+    """
 
     def print_sorted(self):
-        """Prints the list in ascending sorted order"""
+        """
+        Prints the list in ascending sorted order
+        """
         print(sorted(self))
