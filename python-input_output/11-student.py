9d0691e9e46c408d5e4056dbcba3210b5475ebf8
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-06-14 17:32:33 +0000

    Fix PEP8 issues and simplify comments in Student class

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Module that defines a Student class with serialization and reloading."""
+"""Student class with serialization and reloading."""
 
 
 class Student:
-    """Defines a student by first name, last name, and age."""
+    """Defines a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns dictionary representation of instance.
-        Filters with attrs list if provided."""
         if isinstance(attrs, list) and all(
             isinstance(attr, str) for attr in attrs
         ):
@@ -24,6 +21,5 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Updates instance attributes from a dictionary."""
         for key, value in json.items():
             setattr(self, key, value)
