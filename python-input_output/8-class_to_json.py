80a4741319c308b5c2709a91c4be7b3790f07e18
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-06-14 16:53:03 +0000

    Fix PEP8: split long docstring and add blank line before function

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Module that provides a function to return dictionary representation of an object."""
+"""Module that provides a function to return dictionary
+representation of an object."""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description of an object for JSON serialization."""
+    """Returns the dictionary description of an object
+    for JSON serialization."""
     return obj.__dict__
