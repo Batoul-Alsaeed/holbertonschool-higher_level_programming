dc0884040d08f734973a9ddbec05e1d361e0bd24
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-06-14 16:22:30 +0000

    Fix PEP8: add blank line before function definition

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that provides a function to append text to a file."""
 
+
 def append_write(filename="", text=""):
     """Appends a string to the end of a UTF8 file and returns char count."""
     with open(filename, "a", encoding="utf-8") as f:
