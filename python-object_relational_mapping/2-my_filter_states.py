911d8830604e74273debbdd0f2db9f18ca1500c4
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-06-29 19:19:39 +0000

    Add: Filter states by exact user input (safe from SQL injection)

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table where name matches the argument.
-Usage: ./2-my_filter_states.py <mysql username> <mysql password> <database name> <state name searched>
+   whose name corresponds to the argument passed
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # Connect to MySQL
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=argv[1],
         passwd=argv[2],
-        db=argv[3],
-        charset="utf8"
-    )
+        db=argv[3])
 
     cur = db.cursor()
-    # Use exact match and prevent SQL injection
-        cur.execute("""SELECT * FROM states
+    cur.execute("""SELECT * FROM states
                  WHERE BINARY name LIKE '{}%'
                  ORDER BY id;""".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
