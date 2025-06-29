2a887df3cce1becf1988511aa269ba289d284c35
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-06-29 17:36:44 +0000

    Task 17: Convert database, table, and name column to UTF8 utf8mb4_unicode_ci

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Change database charset and collation to utf8mb4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Change table charset and collation to utf8mb4
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Change column 'name' collation only (optional if covered above, but safe to include)
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
