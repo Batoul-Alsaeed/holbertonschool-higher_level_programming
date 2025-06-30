ca7744d1ce662e4de1b84fcd7976d18bfc56365c
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-06-30 17:06:30 +0000

    Fix: Join and format output for task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Fetch all cities and their states from DB"""
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+"""
+
 import sys
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    # Setup engine and session
+
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Join states and cities and sort by city.id
-    results = session.query(City, State).join(State).order_by(City.id).all()
-    for city, state in results:
-        print(f"{state.name}: ({city.id}) {city.name}")
+    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
+    for state, city in st_cty:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
