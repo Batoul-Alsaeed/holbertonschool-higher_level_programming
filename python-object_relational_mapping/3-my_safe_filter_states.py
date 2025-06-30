510803310d6a1a106c90343fe55aca66c2b1ceb4
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-06-30 15:16:24 +0000

    Task 3: Safely filter states using parameterized queries to prevent SQL injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""
-Safely displays all values in the states table where name matches the user input.
-Prevents SQL injection by using parameterized queries.
-Usage: ./3-my_safe_filter_states.py <mysql username> <mysql password> <database name> <state name searched>
-"""
+"""Safely filters states by name using parameterized query"""
 
 import MySQLdb
-from sys import argv
-
+import sys
 
 if __name__ == "__main__":
-    connexion = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
+    # Read arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    argv_4 = argv[4].split(';')[0]
+    # Connect to database
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
 
-    curs = connexion.cursor()
-    curs.execute("""SELECT * FROM states
-                 WHERE BINARY name LIKE '{}%'
-                 ORDER BY id;""".format(argv_4))
-    rows = curs.fetchall()
-    for row in rows:
+    # Create cursor and execute safe query
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
+
+    # Print results
+    for row in cur.fetchall():
         print(row)
-    curs.close()
-    connexion.close()
+
+    # Cleanup
+    cur.close()
+    db.close()
