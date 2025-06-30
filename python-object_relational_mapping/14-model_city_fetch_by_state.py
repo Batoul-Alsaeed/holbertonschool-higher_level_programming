4316ebb6dba833abccea84146012f2dfc8fd9d93
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-06-30 16:04:59 +0000

    Add City model and script to fetch all cities with states

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Fetch all cities and their states from DB"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Setup engine and session
+    engine = create_engine(
+        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}",
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Join states and cities and sort by city.id
+    results = session.query(City, State).join(State).order_by(City.id).all()
+    for city, state in results:
+        print(f"{state.name}: ({city.id}) {city.name}")
