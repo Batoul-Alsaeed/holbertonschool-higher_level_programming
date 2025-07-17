db41399de4cadf9c0262753f75430703a88f31b1
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-07-17 17:21:44 +0300

    Task 7: Fetch and display Star Wars movie titles

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,12 @@
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const movies = data.results;
+    const ul = document.querySelector('#list_movies');
+
+    movies.forEach(movie => {
+      const li = document.createElement('li');
+      li.textContent = movie.title;
+      ul.appendChild(li);
+    });
+  });
